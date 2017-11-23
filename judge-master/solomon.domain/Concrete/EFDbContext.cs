3344e0f82b7394a9f7e0811963ccf363023af069
Josef Dzeranov <soslan29@gmail.com>
2017-11-23 22:02:36 +0300

    Add tags

--- judge-master/solomon.domain/Concrete/EFDbContext.cs
+++ judge-master/solomon.domain/Concrete/EFDbContext.cs
@@ -57,7 +57,6 @@ namespace Solomon.Domain.Concrete
     {
         protected override void Seed(EFDbContext context)
         {
-
             IList<ProgrammingLanguage> defaultLanguages = new List<ProgrammingLanguage>();
 
             defaultLanguages.Add(new ProgrammingLanguage
@@ -99,45 +98,74 @@ namespace Solomon.Domain.Concrete
             foreach (var std in defaultLanguages)
                 context.ProgrammingLanguages.Add(std);
 
-            context.UserCategories.Add(new UserCategory { UserCategoryID = UserCategories.None, Name = "Не выбрано" });
-            context.UserCategories.Add(new UserCategory { UserCategoryID = UserCategories.School, Name = "Школьник" });
-            context.UserCategories.Add(new UserCategory { UserCategoryID = UserCategories.Student, Name = "Студент" });
+            context.UserCategories.Add(new UserCategory {UserCategoryID = UserCategories.None, Name = "Не выбрано"});
+            context.UserCategories.Add(new UserCategory {UserCategoryID = UserCategories.School, Name = "Школьник"});
+            context.UserCategories.Add(new UserCategory {UserCategoryID = UserCategories.Student, Name = "Студент"});
             context.UserCategories.Add(new UserCategory
             {
                 UserCategoryID = UserCategories.Teacher,
                 Name = "Преподаватель"
             });
-            context.UserCategories.Add(new UserCategory { UserCategoryID = UserCategories.Other, Name = "Другое" });
-
+            context.UserCategories.Add(new UserCategory {UserCategoryID = UserCategories.Other, Name = "Другое"});
 
 
             context.TournamentTypes.Add(
-                new TournamentType { TournamentTypeID = TournamentTypes.Open, Name = "Открытый" });
+                new TournamentType {TournamentTypeID = TournamentTypes.Open, Name = "Открытый"});
             context.TournamentTypes.Add(
-                new TournamentType { TournamentTypeID = TournamentTypes.Close, Name = "Закрытый" });
+                new TournamentType {TournamentTypeID = TournamentTypes.Close, Name = "Закрытый"});
 
-            context.TournamentFormats.Add(new TournamentFormat { TournamentFormatID = TournamentFormats.ACM, Name = "ACM (Проверка до первого неверного ответа, штрафное время за посылки)" });
-            context.TournamentFormats.Add(new TournamentFormat { TournamentFormatID = TournamentFormats.IOI, Name = "IOI (Проверка на всех тестах, без штрафных очков)" });
+            context.TournamentFormats.Add(new TournamentFormat
+            {
+                TournamentFormatID = TournamentFormats.ACM,
+                Name = "ACM (Проверка до первого неверного ответа, штрафное время за посылки)"
+            });
+            context.TournamentFormats.Add(new TournamentFormat
+            {
+                TournamentFormatID = TournamentFormats.IOI,
+                Name = "IOI (Проверка на всех тестах, без штрафных очков)"
+            });
 
-            context.ProblemTypes.Add(new ProblemType() { ProblemTypeID = ProblemTypes.Interactive, Name = "Интерактивная" });
-            context.ProblemTypes.Add(new ProblemType() { ProblemTypeID = ProblemTypes.Open, Name = "Открытая задача (только ответ на задачу)" });
-            context.ProblemTypes.Add(new ProblemType() { ProblemTypeID = ProblemTypes.Standart, Name = "Стандартная задача (стандартные потоки ввода/вывода)" });
+            context.ProblemTypes.Add(new ProblemType
+            {
+                ProblemTypeID = ProblemTypes.Interactive,
+                Name = "Интерактивная"
+            });
+            context.ProblemTypes.Add(new ProblemType
+            {
+                ProblemTypeID = ProblemTypes.Open,
+                Name = "Открытая задача (только ответ на задачу)"
+            });
+            context.ProblemTypes.Add(new ProblemType
+            {
+                ProblemTypeID = ProblemTypes.Standart,
+                Name = "Стандартная задача (стандартные потоки ввода/вывода)"
+            });
 
-            context.TestResults.Add(new TestResult { TestResultID = TestResults.CE, Name = "CE" });
-            context.TestResults.Add(new TestResult { TestResultID = TestResults.CHKP, Name = "CHKP" });
-            context.TestResults.Add(new TestResult { TestResultID = TestResults.Compiling, Name = "Compiling" });
-            context.TestResults.Add(new TestResult { TestResultID = TestResults.Disqualified, Name = "Disqualified" });
-            context.TestResults.Add(new TestResult { TestResultID = TestResults.Executing, Name = "Executing" });
-            context.TestResults.Add(new TestResult { TestResultID = TestResults.FL, Name = "FL" });
-            context.TestResults.Add(new TestResult { TestResultID = TestResults.MLE, Name = "MLE" });
-            context.TestResults.Add(new TestResult { TestResultID = TestResults.OK, Name = "OK" });
-            context.TestResults.Add(new TestResult { TestResultID = TestResults.PE, Name = "PE" });
-            context.TestResults.Add(new TestResult { TestResultID = TestResults.PS, Name = "PS" });
-            context.TestResults.Add(new TestResult { TestResultID = TestResults.Waiting, Name = "Waiting" });
-            context.TestResults.Add(new TestResult { TestResultID = TestResults.TLE, Name = "TLE" });
-            context.TestResults.Add(new TestResult { TestResultID = TestResults.RTE, Name = "RTE" });
-            context.TestResults.Add(new TestResult { TestResultID = TestResults.WA, Name = "WA" });
-            
+            context.TestResults.Add(new TestResult {TestResultID = TestResults.CE, Name = "CE"});
+            context.TestResults.Add(new TestResult {TestResultID = TestResults.CHKP, Name = "CHKP"});
+            context.TestResults.Add(new TestResult {TestResultID = TestResults.Compiling, Name = "Compiling"});
+            context.TestResults.Add(new TestResult {TestResultID = TestResults.Disqualified, Name = "Disqualified"});
+            context.TestResults.Add(new TestResult {TestResultID = TestResults.Executing, Name = "Executing"});
+            context.TestResults.Add(new TestResult {TestResultID = TestResults.FL, Name = "FL"});
+            context.TestResults.Add(new TestResult {TestResultID = TestResults.MLE, Name = "MLE"});
+            context.TestResults.Add(new TestResult {TestResultID = TestResults.OK, Name = "OK"});
+            context.TestResults.Add(new TestResult {TestResultID = TestResults.PE, Name = "PE"});
+            context.TestResults.Add(new TestResult {TestResultID = TestResults.PS, Name = "PS"});
+            context.TestResults.Add(new TestResult {TestResultID = TestResults.Waiting, Name = "Waiting"});
+            context.TestResults.Add(new TestResult {TestResultID = TestResults.TLE, Name = "TLE"});
+            context.TestResults.Add(new TestResult {TestResultID = TestResults.RTE, Name = "RTE"});
+            context.TestResults.Add(new TestResult {TestResultID = TestResults.WA, Name = "WA"});
+
+            context.ProblemTags.Add(new ProblemTag {ProblemTagID = 0, Name = "Ввод вывод данных"});
+            context.ProblemTags.Add(new ProblemTag {ProblemTagID = 1, Name = "Условный оператор"});
+            context.ProblemTags.Add(new ProblemTag {ProblemTagID = 2, Name = "Циклы"});
+            context.ProblemTags.Add(new ProblemTag {ProblemTagID = 3, Name = "Массивы"});
+            context.ProblemTags.Add(new ProblemTag {ProblemTagID = 4, Name = "Строки"});
+            context.ProblemTags.Add(new ProblemTag {ProblemTagID = 5, Name = "Функции"});
+            context.ProblemTags.Add(new ProblemTag {ProblemTagID = 6, Name = "Рекурсия"});
+            context.ProblemTags.Add(new ProblemTag {ProblemTagID = 7, Name = "Динамика"});
+            context.ProblemTags.Add(new ProblemTag {ProblemTagID = 8, Name = "Бинарный поиск"});
+
             //All standards will
             base.Seed(context);
         }
